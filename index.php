3f314062f019e0dcf48cfef84c054695f7a5502d
Nehalshaikh8698 <nehalshaikh02017@gmail.com>
2025-07-26 16:24:18 +0530

    Added favicon and updated styles and head section

--- index.php
+++ index.php
@@ -1,107 +1,77 @@
 <?php
 $insert = false;
-if(isset($_POST['name'])){
+if (isset($_POST['name'])) {
 
-$server ="localhost";
-$username = "root";
-$password = "";
+    $server = "localhost";
+    $username = "root";
+    $password = "";
 
-$con = mysqli_connect($server, $username, $password);
+    $con = mysqli_connect($server, $username, $password);
 
-if(!$con){
-    die("connection to this databse failed due to ".
-    mysqli_connect_error());
-}
-// echo "Success connecting to the db"
+    if (!$con) {
+        die("Connection to this database failed due to " . mysqli_connect_error());
+    }
 
-$name=$_POST['name'];
-$gender=$_POST['gender'];
-$age = $_POST['age'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$desc = $_POST['desc'];
+    $name   = $_POST['name'];
+    $gender = $_POST['gender'];
+    $age    = $_POST['age'];
+    $email  = $_POST['email'];
+    $phone  = $_POST['phone'];
+    $desc   = $_POST['desc'];
 
-$sql =  "INSERT INTO `trip_form`.`trip` ( `name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) 
-     VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', 
-     current_timestamp());";
+    $sql = "INSERT INTO `trip_form`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) 
+            VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
 
-    //  echo $sql;
+    if ($con->query($sql) == true) {
+        $insert = true;
+    } else {
+        echo "ERROR: $sql <br> $con->error";
+    }
 
-if($con->query($sql) == true){
-    // echo " Successfully Inserted";
-    $insert = true;
-
-
-}     
-else{
-    echo "ERROR: $sql <br> $con->error";
-}
-$con->close();
+    $con->close();
 }
 ?>
 
-
-
-<!-- html -->
+<!-- HTML -->
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
-    <title>Welcome To Travel Form</title>
+    <title>Let's Connect!</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Intel+One+Mono:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <img class="bg" src="bg.jpg" alt="DYPCOE">
-    <div class="container">
-        <h1>Welcome to US trip Form </h1>
-        <p>Enter Your Details</p>
-        <?php
-        if($insert == true){
-         echo " <p class='submitmsg'>thanks for submiting form</p> ";
-        }
-?>
-          <form action="index.php" method="post">
-        <input type="text" 
-        name="name"
-        id="name"
-        placeholder="Enter Your Name"
-        >
-         <input type="text" 
-        name="age"
-        id="age"
-        placeholder="Enter Your Age"
-        >
-          <input type="text" 
-        name="gender"
-        id="gender"
-        placeholder="Enter Your Gender"
-        >
-          <input type="text" 
-        name="email"
-        id="email"
-        placeholder="Enter Your Email"
-        >
-          <input type="phone" 
-        name="phone"
-        id="phone"
-        placeholder="Enter Your Phone Number"
-        >
-        <textarea
-        name="desc"
-        id="desc"
-        cols="30"
-        rows="10"
-        placeholder="Enter More Details"
-        >
-        </textarea>
-        <button class="btn">Submit</button>
-    </form>
-    </div>
-    <script src="index.js"></script>
 
-  </body>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Intel+One+Mono:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+
+</head>
+<body>
+    <img class="bg" src="bg.jpg" alt="DYPCOE">
+
+    <?php if ($insert == true): ?>
+        <div class="submit-msg">
+            <p class="submitmsg">🎉 Thank you for submitting your details! We’ll be in touch soon.</p>
+        </div>
+    <?php else: ?>
+        <div class="container">
+            <h1>Want to connect? Just share your basic details below</h1>
+            <p>Tell us a little about yourself to begin the journey</p>
+
+            <form action="index.php" method="post">
+                <input type="text" name="name" id="name" placeholder="Enter Your Name">
+                <input type="text" name="age" id="age" placeholder="Enter Your Age">
+                <input type="text" name="gender" id="gender" placeholder="Enter Your Gender">
+                <input type="text" name="email" id="email" placeholder="Enter Your Email">
+                <input type="phone" name="phone" id="phone" placeholder="Enter Your Phone Number">
+                <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter More Details"></textarea>
+                <button class="btn">Submit</button>
+            </form>
+        </div>
+    <?php endif; ?>
+
+    <script src="index.js"></script>
+</body>
 </html>
